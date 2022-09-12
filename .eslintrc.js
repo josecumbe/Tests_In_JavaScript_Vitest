bc791702b5b8f5eea7236cad15024cde282098c5
Jose Cumbe <joseorlando182@hotmail.com>
2022-09-12 17:28:40 +0200

    created node setup

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-useless-constructor": "off"
+    }
+}
